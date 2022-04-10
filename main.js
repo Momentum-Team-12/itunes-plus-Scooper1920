424b51f8f29ab42c3134287e617f9b24aab0c81f
Shawna Cooper <scooper1920@gmail.com>
2022-04-10 18:16:53 -0400

    (on a roll with the homework)

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log("Holllaaa!")
 
-let container = document.querySelector("#container")
+let root = document.querySelector("#root")
 
-fetch("https://itunes.apple.com/search?term=radiohead&media=music", {
+fetch("https://itunes.apple.com/search?term=barenaked+ladies&media=music", {
   method: "GET",
 })
 
@@ -11,13 +11,47 @@ fetch("https://itunes.apple.com/search?term=radiohead&media=music", {
 })
 
 .then(function(data){
-    for (let i of data.results.slice(1)) { 
+    for (let i of data.results) { 
         let infodiv = document.createElement("div")
-        container.appendChild(infodiv)
+        infodiv.classList.add("info")
+        root.appendChild(infodiv)
         infodiv.innerText = i.trackName
+
+let picDiv = document.createElement("img")
+root.appendChild(picDiv)
+picDiv.src = i.artworkUrl100
+picDiv.classList.add("pic")
+
+
+let playerDiv = document.createElement("div")
+root.appendChild(playerDiv)
+playerDiv.classList.add("player")
+playerDiv.innerHTML =`
+        <figure>
+    <figcaption>Listen to this track:</figcaption>
+    <audio
+        controls
+        src="${i.previewUrl}">
+             Your browser does not support the
+             <code>audio</code> element.
+     </audio>
+ </figure>
+
+         `
         console.log(i.trackName)
         console.log("div created")
+
+        console.log(i.previewUrl)
         
     }
 
+
 })
+
+
+
+// .addEventListener('keypress', function (e) {
+//     if (e.key === 'Enter') {
+//       // code for enter
+//     }
+// });
