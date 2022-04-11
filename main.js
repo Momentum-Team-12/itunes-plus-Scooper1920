9c486fafebbda72c021f5c5391d6fe94c16d4ed9
Shawna Cooper <scooper1920@gmail.com>
2022-04-10 21:23:49 -0400

    (commented some stuff to try to get the search bar working)

--- main.js
+++ main.js
@@ -63,22 +63,15 @@ playerDiv.innerHTML =`
 
 })
 
-// let input = document.getElementById("search-input")
-// submit.addEventListener("click",() => {
-//     fetch(`https://itunes.apple.com/search?term=${input.value}&media=music`, {
+
+// searchInput.addEventListener("click", (e) => {
+//     let value = e.target.value
+
+//     if (value && value.trim().length > 0){
+//          value = value.trim().toLowerCase()
+
+//          fetch(`https://itunes.apple.com/search?term=${value}&media=music`, {
 //   method: "GET",
-  
-// })
-// console.log("clicked")
-
-// })
-
-// submit.addEventListener("click",() => {
-//     fetch(`https://itunes.apple.com/search?term=${input.value}&media=music`, {
-//   method: "GET",
-
-  
-  
 // })
 
 // .then(function(response) {
@@ -86,9 +79,56 @@ playerDiv.innerHTML =`
 // })
 
 // .then(function(data){
-// console.log("clicked")
+//     for (let i of data.results.slice(2)) { 
+
+//         let bandCard = document.createElement("div")
+//         root.appendChild(bandCard)
+//         bandCard.id = "bandCard"
+        
+//         let info = document.createElement("p")
+//         info.classList.add("info")
+//         bandCard.appendChild(info)
+//         info.innerText = i.trackName
+
+
+
+// let picDiv = document.createElement("img")
+// bandCard.appendChild(picDiv)
+// picDiv.src = i.artworkUrl100
+// picDiv.classList.add("pic")
+
+
+// let playerDiv = document.createElement("div")
+// bandCard.appendChild(playerDiv)
+// playerDiv.classList.add("player")
+// playerDiv.innerHTML =`
+//         <figure>
+//     <figcaption>Listen to ${i.trackName}:</figcaption>
+//     <audio
+//         controls
+//         src="${i.previewUrl}">
+//              Your browser does not support the
+//              <code>audio</code> element.
+//      </audio>
+//  </figure>
+
+//          `
+//         console.log(i.trackName)
+//         console.log("div created")
+
+//         console.log(i.previewUrl)
+        
+//     }
+
 
 // })
+
+
+
+
+  
+  
+
 
 
 
